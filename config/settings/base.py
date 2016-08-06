498e6ac38e63ae8591dcee669838b60e2069fd70
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-08-05 23:58:03 -0500

    Add departments module to project

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,6 +44,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'bricksa.apps.projects',
+    'bricksa.apps.departments',
 )
 
 INSTALLED_APPS = THIRD_PARTY_APPS + DJANGO_APPS + LOCAL_APPS
