60a073446840a04321ba06fd60a59f11466167a1
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-08-31 15:46:10 -0500

    #5, Add project detail view

--- bricksa/apps/projects/views.py
+++ bricksa/apps/projects/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from bricksa.apps.projects.models import Project
 
@@ -10,3 +10,10 @@ class ProjectListView(ListView):
     template_name = 'projects/project_list.html'
     model = Project
     context_object_name = 'projects'
+
+
+class ProjectDetailView(DetailView):
+    template_name = 'projects/project_detail.html'
+    model = Project
+    pk_url_kwarg = 'id'
+    context_object_name = 'project'
