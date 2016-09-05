207b677b118e98e0e6924cd87119e5375d552fd9
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-09-04 20:24:52 -0500

    #6, Add validation on embeded saved link

--- bricksa/apps/projects/forms.py
+++ bricksa/apps/projects/forms.py
@@ -14,9 +14,12 @@ class ProjectForm(forms.ModelForm):
     def clean_video(self):
         cleaned_data = super(ProjectForm, self).clean()
         video = cleaned_data.get('video')
-        try:
-            arg = video.split('=')[1]
-        except:
-            raise forms.ValidationError('Inserte un link de la forma https://www.youtube.com/watch?v=IVx6ZlksMJw')
-        video_embedded = 'https://www.youtube.com/embed/%s' % arg
+        if 'embed' in video:
+            video_embedded = video
+        else:
+            try:
+                arg = video.split('=')[1]
+                video_embedded = 'https://www.youtube.com/embed/%s' % arg
+            except:
+                raise forms.ValidationError('Inserte un link de la forma https://www.youtube.com/watch?v=IVx6ZlksMJw')
         return video_embedded
