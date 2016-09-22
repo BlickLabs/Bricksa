203e618c5a3ce6a0ef211c3dfbf39e37b37a27d7
Alan Sanchez <sanchezpineda03@gmail.com>
2016-09-22 00:42:16 -0500

    Implemented ajax request to project brochure.

---
+++ bricksa/static/app/js/download_brochure.js
@@ -0,0 +1,36 @@
+var addBrochureListener = (function () {
+  'use strict';
+  /*global djangoURLs, ga*/
+  return function (gaEventName, developmentID) {
+    $('#download-brochure-button').click(function (e) {
+      e.preventDefault();
+      var button = $(this),
+        span = $('#download-brochure-span');
+      $.ajax({
+        url: djangoURLs.downloadBrochure + developmentID + '/',
+        type : 'GET',
+        data: $('#download-brochure-form').serialize(),
+        beforeSend: function () {
+          var isEmail = /^[\-a-z0-9~!$%\^&*_=+}{\'?]+(\.[\-a-z0-9~!$%\^&*_=+}{\'?]+)*@([a-z0-9_][\-a-z0-9_]*(\.[\-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+          if (isEmail.test($('#download-brochure-email').val())) {
+            button.addClass('disabled');
+            span.text('');
+            span.css('margin-top', '0');
+            span.css('margin-bottom', '');
+            // ga('send', 'event', gaEventName, 'ok');
+          } else {
+            span.css('margin-top', '20px');
+            span.css('margin-bottom', '-20px');
+            span.text('Introduce una dirección de correo válida.');
+            return false;
+          }
+        },
+        complete: function () {
+          button.removeClass('disabled');
+          span.text('');
+          window.location.href = djangoURLs.downloadBrochure + developmentID;
+        }
+      });
+    });
+  };
+}());
