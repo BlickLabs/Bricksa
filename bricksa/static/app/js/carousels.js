ceed6ea5684513b0f7740dc2ba09006dbe8bb4c6
Alan Sanchez <sanchezpineda03@gmail.com>
2016-10-16 20:03:25 -0500

    Modified slick carousel for project photos: Shown just one photo in small devices.

--- bricksa/static/app/js/carousels.js
+++ bricksa/static/app/js/carousels.js
@@ -8,5 +8,13 @@
     prevArrow: '', 
     nextArrow: '<a href="#" class="arrow-next"></a>',
     slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 })();
