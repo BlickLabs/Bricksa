1324f311427aa2ecbe44c7482af193162c42c117
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2016-11-17 14:37:44 -0600

    #29, Add migrations to office model

---
+++ bricksa/apps/offices/models.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from bricksa.apps.projects.models import Project
+
+
+class Office(models.Model):
+    project = models.OneToOneField(
+        Project,
+        verbose_name=_('Project')
+    )
+    office_floors = models.IntegerField(
+        _('Office floors'),
+        blank=False,
+        null=False,
+        default=0
+    )
+    bathrooms_per_level = models.IntegerField(
+        _('Bathrooms per level'),
+        blank=False,
+        null=False,
+        default=0
+    )
+    elevator= models.IntegerField(
+        _('Elevator'),
+        blank=False,
+        null=False,
+        default=0
+    )
